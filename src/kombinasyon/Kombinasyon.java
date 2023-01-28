d885e86bc5036fdec0c48c9e0ccce1c04a2c185d
Muhammed Furkan Kocer <muhammedfurkankocer@gmail.com>
2023-01-29 00:30:54 +0300

    v7

--- src/kombinasyon/Kombinasyon.java
+++ src/kombinasyon/Kombinasyon.java
@@ -9,7 +9,7 @@ public class Kombinasyon {
         ArrayList<String> arrList = new ArrayList<>(Arrays.asList("A","B","C","D","E"));
         for (int i = 0; i < arrList.size(); i++) {
             for (int j = i + 1; j < arrList.size(); j++) {
-                System.out.println(arrList.get(i) + " " + arrList.get(j));
+                System.out.print(arrList.get(i) + " " + arrList.get(j)+", ");
             }
         }
 
